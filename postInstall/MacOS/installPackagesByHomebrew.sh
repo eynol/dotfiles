7b91aa24813cc91fb855174e702514c753190473
eynol <625608852@qq.com>
2021-05-25 13:27:03 +0800

    update packages

--- postInstall/MacOS/installPackagesByHomebrew.sh
+++ postInstall/MacOS/installPackagesByHomebrew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-brew install wget ncdu nvm nnn  whois aria2 cmake emacs ffmpeg gcc fzf htop jq tmux  zsh zsh-autosuggestions
+brew install wget ncdu nvm nnn  whois aria2 cmake emacs ffmpeg gcc fzf htop jq tmux  zsh zsh-autosuggestions mosh tldr youtube-dl   the_silver_searcher sshfs
 brew install --cask maccy adobe-acrobat-reader  airdroid alfred android-file-transfer audacity avibrazil-rdm blender  calibre docker  drawio figma figmadaemon free-download-manager gimp github go2shell iina inkscape iterm2  kap macvim 
-brew install --cask openvpn-connect snipaste sourcetree spotify  syncthing  synology-note-station-client the-unarchiver  wireshark xmind zerotier-one 
+brew install --cask openvpn-connect snipaste sourcetree spotify  syncthing  synology-note-station-client the-unarchiver  wireshark xmind zerotier-one kicad mounty
