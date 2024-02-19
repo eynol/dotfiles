d20a90fc41bd3d55f67bb5c3d2d8184f0fd34cee
eynol <eynol@qq.com>
2024-02-20 01:29:56 +0800

    update deno config

--- .shell.rc.sh
+++ .shell.rc.sh
@@ -15,6 +15,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
+# deno config
+export DENO_INSTALL="$HOME/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
 # fzf config
 if [ -n "$ZSH_VERSION" ]; then
   # assume Zsh
