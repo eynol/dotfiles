4a71f39d3c962be9d8cf56ddd6729181cb31207f
eynol <eynol@qq.com>
2024-02-21 02:35:17 +0800

    update

--- setup.sh
+++ setup.sh
@@ -3,27 +3,14 @@
 echo "Please select config names to install"
 
 DIR_NAME=$(cd $(dirname $0);pwd)
-ls $DIR_NAME/.nvim-profiles
-select name in "nvim" "zsh" "tmux" "ranger"
+
+NVIM_PATH=$HOME/.config/nvim
+
+
+select name in $(ls $DIR_NAME/.nvim-profiles)
 do
-    case $name in
-        "nvim")
-            ln -v -f -s $PWD/nvim $HOME/.config/nvim
-            break
-            ;;
-        "zsh")
-            echo "no"
-            break
-            ;;
-        "tmux")
-            echo "no"
-            break
-            ;;
-        "ranger")
-            echo "no"
-            break
-            ;;
-        *)
-            echo "Incorrect choice, exit"
-    esac
+
+    ln -vsnf $DIR_NAME/.nvim-profiles/$name $NVIM_PATH
+    break
+    
 done
