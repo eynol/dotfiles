bcfa306045ac170b240271e05595ad8289dbabf4
eynol <625608852@qq.com>
2024-02-27 18:56:24 +0800

    update lazyvim lsp hover action,nmap gh :lua vim.lsp.buf.hover()

--- .nvim-profiles/lazyvim/lua/config/keymaps.lua
+++ .nvim-profiles/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- use `vim.keymap.set` instead
+local map = vim.keymap.set
+
+map("n", "gh", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "Try get hover info" })
