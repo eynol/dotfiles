684b16dd65a088d0087d7f50b433307dd2f45f3e
eynol <625608852@qq.com>
2025-09-29 19:41:45 +0800

    add neovide keymap

--- .nvim-profiles/lazyvim/lua/config/keymaps.lua
+++ .nvim-profiles/lazyvim/lua/config/keymaps.lua
@@ -7,6 +7,14 @@ local map = vim.keymap.set
 
 map("n", "gh", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "Try get hover info" })
 
+if vim.g.neovide then
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", "<C-R>+") -- Paste insert mode
+end
 -- if vim.fn.getenv("USER") == "bytedance" then
 --   local get_git_root_dir = function()
 --     local git_root = vim.fn.system({ "git", "rev-parse", "--show-toplevel" })
